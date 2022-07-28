b30b229a8760e8a70705230c2f5b72587c7909e1
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-07-29 01:33:23 +0600

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-    // your solution
+
+  let condition = bracketsConfig.some(([openBracket, closeBracket]) => {
+    return str.includes(openBracket + closeBracket)
+  })
+
+  while (condition) {
+    bracketsConfig.forEach(([openBracket, closeBracket]) => {
+      str = str.replaceAll(openBracket + closeBracket, '')
+    });
+
+    condition = bracketsConfig.some(([openBracket, closeBracket]) => {
+      return str.includes(openBracket + closeBracket)
+    })
+  }
+
+  return str.length === 0
 }
